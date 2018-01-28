0943743cdf637d3e13b67acae98e47f65b8bd9cc
Steven Uray <steven.uray@gmail.com>
2018-01-27 19:20:25 -0800

    Initial Visitor controller.

---
+++ app/controllers/visitor_controller.rb
@@ -0,0 +1,13 @@
+class VisitorController < ApplicationController
+  def create
+  end
+
+  def read
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'visitor/create'
+
+  get 'visitor/read'
+
+  get 'visitor/update'
+
+  get 'visitor/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
