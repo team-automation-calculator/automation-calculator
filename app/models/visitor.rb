6c0399a362c2036ea7cea3ba0ac8fc09a80d043c
Steven Uray <steven.uray@gmail.com>
2018-01-27 17:49:44 -0800

    Validations for Visitor model.

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,3 +1,4 @@
 class Visitor < ApplicationRecord
   has_many :automation_scenarios, as: :owner, dependent: :destroy
+  validates :ip, :uuid, presence: true
 end
--- spec/models/visitor_spec.rb
+++ spec/models/visitor_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Visitor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:visitor) { create(:visitor) }
+
+  describe 'validations' do
+    context 'with valid model' do
+      it 'should be valid' do
+        expect(visitor.valid?).to be_truthy
+      end
+    end
+
+    it { should validate_presence_of(:ip) }
+    it { should validate_presence_of(:uuid) }
+  end
 end
