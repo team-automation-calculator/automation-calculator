554e32576546fa3fb186ddc99ad8d9077b8bf16c
Konstantin Rafalsky <kraft001@gmail.com>
2018-01-31 14:14:34 +0400

    Reset warden helpers for tests

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,5 +1,9 @@
 require 'devise'
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+
   include Warden::Test::Helpers
+  config.after do
+    Warden.test_reset!
+  end
 end
