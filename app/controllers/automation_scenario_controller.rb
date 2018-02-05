8a197c34791eccd4c4ee3f1b5a5997b89855b83b
Steven Uray <steven.uray@gmail.com>
2018-02-04 17:45:33 -0800

    Removing automation_scenario_controller until it is used.

--- app/controllers/automation_scenario_controller.rb
+++ app/controllers/automation_scenario_controller.rb
@@ -1,5 +1,17 @@
 class AutomationScenarioController < ApplicationController
   def create
-    #
+    Visitor.create_with_random_uuid(visitor_params)
+  end
+
+  def read
+    #fill in read
+  end
+
+  def update
+    #fill in read
+  end
+
+  def delete
+    #fill in read
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
 
-  post 'automation_scenario/create'
-
-  get 'automation_scenario/read'
-
-  put 'automation_scenario/update'
-
-  delete 'automation_scenario/delete'
-
-  post 'visitor/create'
-
-  get 'visitor/read'
-
-  put 'visitor/update'
-
-  delete 'visitor/delete'
+  resources :visitor
 
   root to: 'home#index'
 end
