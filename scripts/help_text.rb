2ab8fa9da86f24ef18b24035520c264620a57d63
Steven Uray <steven.uray@gmail.com>
2018-02-10 18:00:42 -0800

    Refactoring ./go help into it's own helper class.

---
+++ scripts/help_text.rb
@@ -0,0 +1,37 @@
+class HelpText
+  HELP_HASH = {
+    build: {
+      dev: 'Build docker containers for your development environment.',
+      ci: 'Build docker containers to simulate the ci environment.'
+    }.freeze,
+    init: 'Setup your development environment with docker.',
+    rm: 'Remove running or stopped docker containers for a clean restart.',
+    rmi: 'Remove the development docker image. ',
+    start: 'Startup the application and it\'s dependencies in docker.',
+    stop: 'Stop running docker containers.',
+    test: 'Run all tests in the docker containers.'
+  }.freeze
+
+  class << self
+    def help(cmds)
+      sub_cmd = cmds.shift
+
+      if sub_cmd.nil?
+        HELP_HASH.each_pair { |key, value| print_help_key_value(key, value) }
+      elsif HELP_HASH.key?(sub_cmd.to_sym)
+        print_help_key_value(sub_cmd.to_sym, HELP_HASH[sub_cmd.to_sym])
+      else
+        warn("Unrecognized command: #{sub_cmd}")
+      end
+    end
+
+    def print_help_key_value(key, value)
+      if value.class == Hash
+        puts(" #{key}:")
+        value.each_pair { |sub_key, sub_value| puts("   #{sub_key} - #{sub_value}") }
+      else
+        puts(" #{key} - #{value}")
+      end
+    end
+  end
+end
