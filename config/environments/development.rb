c1fae80ba647a787b03334579b016b7627bdfc5d
Steven Uray <steven.uray@gmail.com>
2018-02-12 20:52:52 -0800

    Allowing all ips to connect to the server in development to reduce connection issues.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow requests from any source in development
+  config.web_console.whiny_requests = false
 end
