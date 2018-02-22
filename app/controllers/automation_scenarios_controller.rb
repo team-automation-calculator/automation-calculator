fdae78d258d01a40b86e2cee0d98487e7a2b0703
Steven Uray <steven.uray@gmail.com>
2018-02-21 21:03:33 -0800

    Outline of automation scenarios controller.

---
+++ app/controllers/automation_scenarios_controller.rb
@@ -0,0 +1,18 @@
+class AutomationScenariosController < ApplicationController
+  def create
+    # @automation_scenario = AutomationScenario.create(request)
+    # redirect_to(action: :show, id: @automation_scenario.id)
+  end
+
+  def show
+    # @automation_scenario = AutomationScenario.find(request.id)
+  end
+
+  def update
+    # @automation_scenario = AutomationScenario.find(request.id)
+  end
+
+  def destroy
+    # @automation_scenario = AutomationScenario.find(request.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'automation_scenarios/create'
+
+  get 'automation_scenarios/show'
+
+  get 'automation_scenarios/update'
+
+  get 'automation_scenarios/destroy'
+
   devise_for :users
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
