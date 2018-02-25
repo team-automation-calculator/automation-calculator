2ecc39bc243d9c065162c7d10af7cfbeaa2c9326
stevenuray <steven.uray@gmail.com>
2018-02-25 03:18:19 +0000

    Completing automation_scenarios_controller outline with basic actions.

--- app/controllers/automation_scenarios_controller.rb
+++ app/controllers/automation_scenarios_controller.rb
@@ -1,18 +1,29 @@
 class AutomationScenariosController < ApplicationController
   def create
-    # @automation_scenario = AutomationScenario.create(request)
-    # redirect_to(action: :show, id: @automation_scenario.id)
+    @automation_scenario = AutomationScenario.create!(creation_params)
+    redirect_to(action: :show, id: @automation_scenario.id)
   end
 
   def show
-    # @automation_scenario = AutomationScenario.find(request.id)
+    @automation_scenario = AutomationScenario.find(automation_scenario_params)
   end
 
   def update
-    # @automation_scenario = AutomationScenario.find(request.id)
+    @automation_scenario = AutomationScenario.find(automation_scenario_params)
   end
 
   def destroy
-    # @automation_scenario = AutomationScenario.find(request.id)
+    @automation_scenario = AutomationScenario.find(automation_scenario_params)
+    @automation_scenario.destroy
+  end
+
+  private
+
+  def creation_params
+    params.require(:automation_scenario_create_params).permit(:owner_type, :owner_id)
+  end
+
+  def automation_scenario_params
+    params.require(:id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
-  get 'automation_scenarios/create'
-
-  get 'automation_scenarios/show'
-
-  get 'automation_scenarios/update'
-
-  get 'automation_scenarios/destroy'
+  root to: 'home#index'
 
   devise_for :users
-  # For details on the DSL available within this file,
-  # see http://guides.rubyonrails.org/routing.html
+
+  resources :automation_scenarios
 
   get '/visitor', to: 'visitor#index'
   get '/visitor/create', to: 'visitor#create'
   get '/visitor/:id', to: 'visitor#show'
   delete 'visitor/:id', to: 'visitor#destroy'
-
-  root to: 'home#index'
 end
