0c0f8f751e628d32ca6491192713a1edaf5e18a4
stevenuray <steven.uray@gmail.com>
2018-03-06 04:57:41 +0000

    Fixing relative path issue with tagging bash scripts

--- scripts/tag_docker_image_with_semver.sh
+++ scripts/tag_docker_image_with_semver.sh
@@ -3,6 +3,6 @@
 REPO=automationcalculator/automation-calculator-rails
 
 latest_image=$REPO":latest"
-semver_tag=$(.././create_docker_image_tag.sh)
+semver_tag=$(./scripts/create_docker_image_tag.sh)
 semver_image=$REPO":"$semver_tag
 docker tag $latest_image $semver_image
