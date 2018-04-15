51b57ce9a07bc064ffac05f5117eb8305a43ea7f
stevenuray <steven.uray@gmail.com>
2018-04-15 19:31:27 +0000

    Building graph lines from saved solutions.

--- app/assets/javascripts/automation_scenarios.js
+++ app/assets/javascripts/automation_scenarios.js
@@ -1,9 +1,36 @@
 document.addEventListener("turbolinks:load", function() {
-  $('.test-button').click(function () {
-      console.log("You clicked test button!");
-  }),
+    //Functions
+    function buildSolutionGraphLinesFromSolutionsArray(savedSolutionsJSONArray){
+        var solutionAxis = [];
 
-  $('.render-chart').click(function () {
-      console.log("You clicked a paragraph!");
-  })
+        savedSolutionsJSONArray.forEach(function(solutionJson){
+            solutionAxis.push(buildGraphLineFromSolution(solutionJson.initial_cost, solutionJson.iteration_cost, solutionJson.iteration_count));
+        });
+
+        return solutionAxis;
+    }
+
+    function buildGraphLineFromSolution(initial_cost, cost_per_iteration, iterations){
+        var xAxisPoints = [];
+        var yAxisPoints = [];
+        for (i = 1; i <= Number(iterations); i++) {
+            xAxisPoints.push(i);
+            yAxisPoints.push(Number(initial_cost)+(Number(i)*Number(cost_per_iteration)));
+        }
+        return {x:xAxisPoints, y:yAxisPoints, type: 'scatter'};
+    }
+
+    function buildSavedSolutionsJSONArray(){
+        return JSON.parse($('#scenarioSolutions').text());
+    }
+
+    //Event Handlers
+    $('.render-chart').click(function(){
+        Plotly.newPlot('renderChart', buildSolutionGraphLinesFromSolutionsArray(buildSavedSolutionsJSONArray()));
+    });
+
+    //Execution
+    buildSolutionGraphLinesFromSolutionsArray(buildSavedSolutionsJSONArray()).forEach(function(graphLine){
+        console.log(graphLine);
+    });
 });
