82e6d23178f468b7be59dcb2039bebbc168996e7
Recker Swartz <reckerswartz@hotmail.com>
2018-04-26 11:58:16 +0530

    Add sign in, sign up and forgot password pages.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
   }
 
   resources :automation_scenarios
