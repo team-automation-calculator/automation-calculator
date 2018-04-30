a2db100b0f4ce14cd4cd054c5e02abfed8eab1ab
Recker Swartz <reckerswartz@hotmail.com>
2018-04-30 13:02:16 +0530

    add all js and css in assets folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,55 @@
 $( document ).on('turbolinks:load', function() {
   // Javascript method's body can be found in assets/js/demos.js
   demo.initDashboardPageCharts();
+  $("#new_session").validate({
+    errorElement: 'span',
+    errorClass: 'desc',
+    rules: {
+      "user[password]": {
+        required: true
+      },
+      "user[email]": {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      "user[email]": {
+        required: "We need your email address to login.",
+        email: "Your email address must be in the format of name@domain.com."
+      },
+      "user[password]": {
+        required: "We need your password to login.",
+      }
+    },
+  });
+  $("#new_user_registration").validate({
+    errorElement: 'span',
+    errorClass: 'desc',
+    rules: {
+      "user[password]": {
+        required: true
+      },
+      "user[password_confirmation]": {
+        required: true,
+        equalTo : "#password"
+      },
+      "user[email]": {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      "user[email]": {
+        required: "We need your email address for sign up.",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      "user[password]": {
+        required: "We need your password for sign up."
+      },
+       "user[password_confirmation]": {
+        required: "We need your password confirmation as you set password field for sign up."
+      }
+    },
+  });
 });
