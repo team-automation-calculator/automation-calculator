d453373b3338167dee548de2b7243ba4cfd0df6e
Konstantin <kraft001@gmail.com>
2018-04-30 07:00:59 +0300

    Move the iteration_count field to scenarios

--- app/models/automation_scenario.rb
+++ app/models/automation_scenario.rb
@@ -1,15 +1,19 @@
 class AutomationScenario < ApplicationRecord
   belongs_to :owner, polymorphic: true
   has_many :solutions, dependent: :destroy
+
+  validates :iteration_count,
+            numericality: { only_integer: true, greater_than: 0 }
 end
 
 # == Schema Information
 #
 # Table name: automation_scenarios
 #
-#  id         :integer          not null, primary key
-#  owner_type :string
-#  owner_id   :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  owner_type      :string
+#  owner_id        :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  iteration_count :integer          default(10)
 #
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -3,7 +3,6 @@ class Solution < ApplicationRecord
 
   validates :initial_cost, numericality: { only_integer: true, greater_than: 0 }
   validates :iteration_cost, numericality: { only_integer: true, greater_than: 0 }
-  validates :iteration_count, numericality: { only_integer: true, greater_than: 0 }
 end
 
 # == Schema Information
@@ -13,7 +12,6 @@ end
 #  id                     :integer          not null, primary key
 #  initial_cost           :integer
 #  iteration_cost         :integer
-#  iteration_count        :integer
 #  automation_scenario_id :integer
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
