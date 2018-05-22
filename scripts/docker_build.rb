c1ad9ab2512de32ad49b6534901a34bbf0dffa7b
stevenuray <steven.uray@gmail.com>
2018-05-22 02:35:20 +0000

    Refactoring ./go build to eliminate a rubocop violation.

--- scripts/docker_build.rb
+++ scripts/docker_build.rb
@@ -5,25 +5,38 @@ class DockerBuild
 
       case sub_cmd
       when 'dev'
-        build_image('automationcalculator_dev:latest', 'Dockerfile.development')
+        build_dev_image
       when 'ci'
-        repo = 'automationcalculator/automation-calculator-rails'
-        image = "#{repo}:latest"
-        build_image(image, 'Dockerfile.ci')
+        build_ci_image
       when 'base'
-        build_image(
-          'automationcalculators/automation-calculator-base:0.0.2',
-          'Dockerfile.base',
-          'circleci',
-          "--build-arg secret_key_base=#{ENV['SECRET_KEY_BASE']}"
-        )
+        build_base_image
       else
         warn "Unrecognized command: #{sub_cmd}"
       end
     end
 
+    def build_base_image
+      build_image(
+        'automationcalculators/automation-calculator-base:0.0.2',
+        'Dockerfile.base',
+        'circleci'
+      )
+    end
+
+    def build_ci_image
+      repo = 'automationcalculator/automation-calculator-rails'
+      image = "#{repo}:latest"
+      build_image(image, 'Dockerfile.ci', 'circleci', "--build-arg secret_key_base=#{ENV['SECRET_KEY_BASE']}")
+    end
+
+    def build_dev_image
+      build_image('automationcalculator_dev:latest', 'Dockerfile.development')
+    end
+
     def build_image(tag, file, username = ENV['USER'], additional_build_arg='')
-      system("docker build -t #{tag} -f #{file} --build-arg username=#{username} #{additional_build_arg} .")
+      sys_call = "docker build -t #{tag} -f #{file} --build-arg username=#{username} #{additional_build_arg} ."
+      puts sys_call
+      system(sys_call)
     end
   end
 end
