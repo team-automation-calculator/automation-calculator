d72ef2f13888a7dd08b6b91815a16133be9f26a3
Konstantin <kraft001@gmail.com>
2018-06-10 00:48:49 +0300

    Fix tests after the upgrade

--- spec/factories_lint_spec.rb
+++ spec/factories_lint_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.context 'with all factories' do
   it 'lints all of them' do
     DatabaseCleaner.cleaning do
-      FactoryGirl.lint traits: true
+      FactoryBot.lint traits: true
     end
   end
 end
