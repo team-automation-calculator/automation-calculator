4959342e494a8ef8c327a630293bd9a444843982
Konstantin <kraft001@gmail.com>
2018-07-02 17:44:43 +0300

    Fix rubocop offences

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.assets.paths <<
+  Rails.root.join('vendor', 'assets', 'fonts')
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
