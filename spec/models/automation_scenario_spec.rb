36702694ac4b39d2534bbfab987e2462a2f36ed6
Konstantin Rafalsky <kraft001@gmail.com>
2018-08-19 10:35:39 +0300

    Test the combinations method

--- spec/models/automation_scenario_spec.rb
+++ spec/models/automation_scenario_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe AutomationScenario, type: :model do
         .is_greater_than(0)
     end
   end
+
+  describe '.solutions_combinations' do
+    before do
+      create_list :solution, solution_count,
+                  automation_scenario: automation_scenario
+    end
+
+    context 'with 2 solutions' do
+      let(:solution_count) { 2 }
+
+      its('solutions_combinations.size') { is_expected.to eq 1 }
+    end
+
+    context 'with 3 solutions' do
+      let(:solution_count) { 3 }
+
+      its('solutions_combinations.size') { is_expected.to eq 3 }
+    end
+  end
 end
