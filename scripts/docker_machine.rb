1fd3d46968040ca6cf0f669b24ce2fc93c6a037f
Steven Uray <stevenuray@users.noreply.github.com>
2019-06-29 11:17:56 -0700

    Docker machine update using credentials file (#79)
    * Explicitly building base image in go init, correctly tagging dev image so docker-compose doesn't try to build it twice.
    
    * Using aws credentials file instead of aws credentials environment variable in docker_machine script.

--- scripts/docker_machine.rb
+++ scripts/docker_machine.rb
@@ -2,12 +2,16 @@
 class DockerMachine
   class << self
     def create_host(cmds)
-      name = cmds.shift || 'automation-calculator-host'
-      aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-      aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-      exec("docker-machine create --driver amazonec2
-        --amazonec2-access-key #{aws_access_key_id}
-        --amazonec2-secret-key #{aws_secret_access_key} #{name}")
+      name = cmds.shift
+      region = cmds.shift
+      commands = [
+          'docker-machine',
+          'create',
+          '--driver amazonec2',
+          "--amazonec2-region #{region}",
+          name
+      ].join(' ')
+      exec(commands)
     end
   end
 end
