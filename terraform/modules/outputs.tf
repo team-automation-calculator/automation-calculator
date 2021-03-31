7ad093a7012953acd2ef10f2af17dca3f288aeaf
stevenuray <steven.uray@gmail.com>
2021-03-31 13:44:50 -0700

    Adding cert data to outputs

--- terraform/modules/outputs.tf
+++ terraform/modules/outputs.tf
@@ -5,3 +5,7 @@ output "eks_cluster_id" {
 output "eks_cluster_api_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
+
+output "eks_cluster_cert_data" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority
+}
