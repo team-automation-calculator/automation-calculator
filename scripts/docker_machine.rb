5dc828284f2c3589ddd0fe8eabac9c7250d11dc5
Steven Uray <steven.uray@gmail.com>
2022-10-20 13:54:07 -0700

    Adding default args to ./go create_host

--- scripts/docker_machine.rb
+++ scripts/docker_machine.rb
@@ -2,8 +2,8 @@
 class DockerMachine
   class << self
     def create_host(cmds)
-      name = cmds.shift
-      region = cmds.shift
+      name = cmds.shift || "automation-calculator-host-#{Time.now.strftime("%Y-%m-%d")}"
+      region = cmds.shift || 'us-west-1'
       commands = [
         'docker-machine',
         'create',
